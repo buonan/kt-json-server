a2358bb81ac6807633bc6189d9a6f740dd8e928e
buonan <buonan@hotmail.com>
2021-06-07 21:32:48 -0700

    Search based on operation

--- app/src/main/kotlin/kt/json/server/helpers/Helpers.kt
+++ app/src/main/kotlin/kt/json/server/helpers/Helpers.kt
@@ -4,32 +4,56 @@ import com.google.gson.internal.LinkedTreeMap
 import java.net.URLDecoder
 import kotlin.text.Regex.Companion.escapeReplacement
 
+class Operator (val operation: String, val value: String)
+
 object Helpers {
     //posts?title=foo&author=smith
-    fun ParamsSplit(qs: String): HashMap<String, String> {
+    fun ParamsSplit(qs: String): HashMap<String, Operator> {
         var substr = qs.substring(qs.indexOf('?') + 1)
         var p = substr.split('&')
-        var pairs = HashMap<String, String>()
+        var pairs = HashMap<String, Operator>()
         p.forEach { it ->
             var sp = it.split(('='))
-            pairs.put(sp[0], sp[1])
+            pairs.put(sp[0], Operator("=", sp[1]))
         }
         return pairs;
     }
 
-    fun SearchHashMap(mapAny: ArrayList<Any>, mapSearchTerms: HashMap<String, String>): Any? {
+    fun SearchHashMap(mapAny: ArrayList<Any>, mapSearchTerms: HashMap<String, Operator>): Any? {
         var found = ArrayList<Any>()
-        for ((sKey, sValue) in mapSearchTerms) {
-            mapAny.let { it ->
-                println("${it}")
-                it.forEach { it2 ->
-                    println("${it2}")
-                    it2.javaClass.kotlin.members.forEach { it3 ->
-                        if (it3.name == sKey) {
-                            var h = it3.call(it2)
-                            val ss = URLDecoder.decode(sValue)
-                            if (h.toString().contains(ss)) {
-                                found.add(it2)
+        for ((sKey, sOpValue) in mapSearchTerms) {
+            when (sKey) {
+                "_sort" -> {
+                    logger.trace("------ sort ------")
+                }
+                "_page" -> {
+                    logger.trace("------ paginate ------")
+                }
+                "_start" -> {
+                    logger.trace("------ slice ------")
+                }
+                "_gte" -> {
+                    logger.trace("------ operators ------")
+
+                }
+                else -> {
+                    mapAny.let { it ->
+                        println("${it}")
+                        it.forEach { it2 ->
+                            println("${it2}")
+                            it2.javaClass.kotlin.members.forEach { it3 ->
+                                if (it3.name == sKey) {
+                                    var h = it3.call(it2)
+                                    val ss = URLDecoder.decode(sOpValue.value)
+                                    val op = sOpValue.operation
+                                    when (op) {
+                                        "=" -> {
+                                            if (h.toString().equals(ss)) {
+                                                found.add(it2)
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
