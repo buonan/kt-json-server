5f94ec5b8e9306d1d51e885b23947fedd40b2541
buonan <buonan@hotmail.com>
2021-07-29 13:19:57 -0700

    Generalize GetObjectType()

--- app/src/main/kotlin/kt/json/server/adapters/fileAdapter.kt
+++ app/src/main/kotlin/kt/json/server/adapters/fileAdapter.kt
@@ -2,35 +2,17 @@ package kt.json.server
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import kt.json.server.Comment
-import kt.json.server.Post
-import kt.json.server.Profile
-import kt.json.server.globalStorageMap
 import java.io.File
 import java.lang.reflect.Type
-import java.util.List.of
-import kotlin.reflect.typeOf
 
 inline fun <reified T : Any> Any.cast(): T {
     return this as T
 }
 
 object FileAdapter {
-    @OptIn(ExperimentalStdlibApi::class)
     fun GetObjectType(className: String): Type? {
-        var objType: Type? = null
-        when (className) {
-            Post::class.qualifiedName -> {
-                objType = object : TypeToken<java.util.ArrayList<Post>>() {}.type
-            }
-            Comment::class.qualifiedName -> {
-                objType = object : TypeToken<java.util.ArrayList<Comment>>() {}.type
-            }
-            Profile::class.qualifiedName -> {
-                objType = object : TypeToken<java.util.ArrayList<Profile>>() {}.type
-            }
-        }
-        return objType
+        val obj = Class.forName(className).getDeclaredConstructor().newInstance()
+        return TypeToken.getParameterized(ArrayList::class.java, obj::class.java).type
     }
 
     fun initStorageMap(className: String) {
@@ -47,7 +29,7 @@ object FileAdapter {
         } else {
             println("$filename file does not exist.")
 
-            // iniialize storage for testing
+            // initialize storage for testing
             globalStorageMap[className] = ArrayList<Any>()
         }
     }
